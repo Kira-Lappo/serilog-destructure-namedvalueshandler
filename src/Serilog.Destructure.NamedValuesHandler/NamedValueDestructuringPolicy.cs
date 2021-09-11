c97cc613a4557c7e98916af3c85527f71c546e06
Kirill Lappo <kirill.lappo@oxagile.com>
2021-09-11 21:35:58 +0300

    added try catch for handlers and omitters

--- src/Serilog.Destructure.NamedValuesHandler/NamedValueDestructuringPolicy.cs
+++ src/Serilog.Destructure.NamedValuesHandler/NamedValueDestructuringPolicy.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Serilog.Core;
+using Serilog.Debugging;
 using Serilog.Events;
 
 namespace Serilog.Destructure.NamedValuesHandler
@@ -109,7 +110,18 @@ namespace Serilog.Destructure.NamedValuesHandler
 
         private bool IsOmitted((string name, object value, Type valueType) _)
         {
-            return _omitHandlers.Any(h => h.Invoke(_.name, _.value, _.valueType));
+            return _omitHandlers.Any(h =>
+            {
+                try
+                {
+                    return h.Invoke(_.name, _.value, _.valueType);
+                }
+                catch (Exception e)
+                {
+                    SelfLog.WriteLine($"Error at omit check, the value is not omitted. Name: {_.name} Type: {_.valueType}. Exception: {e}");
+                    return false;
+                }
+            });
         }
 
         private static LogEventPropertyValue CreateEventPropertyValue(
@@ -125,7 +137,18 @@ namespace Serilog.Destructure.NamedValuesHandler
         private object HandleNamedValue(string name, object value, Type valueType)
         {
             var handleResult = _namedValueHandlers
-                .Select(h => h.Invoke(name, value, valueType))
+                .Select(h =>
+                {
+                    try
+                    {
+                        return h.Invoke(name, value, valueType);
+                    }
+                    catch (Exception e)
+                    {
+                        SelfLog.WriteLine($"Error at handling value, the value is not modified. Name: {name} Type: {valueType}. Exception: {e}");
+                        return default;
+                    }
+                })
                 .FirstOrDefault(r => r.IsHandled);
 
             return handleResult == default
