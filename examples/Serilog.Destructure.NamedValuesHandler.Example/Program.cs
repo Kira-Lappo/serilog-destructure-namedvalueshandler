7693aeacb4142f2da0aaa0bfc5afea77ff0665ba
Kirill Lappo <kirill-lappo@outlook.com>
2021-09-17 01:58:08 +0300

    added output as a comment message

--- examples/Serilog.Destructure.NamedValuesHandler.Example/Program.cs
+++ examples/Serilog.Destructure.NamedValuesHandler.Example/Program.cs
@@ -30,6 +30,24 @@ namespace Serilog.Destructure.NamedValuesHandler.Example
             logger.Information("Property value is NOT replaced by its type 'string' (with cast to object): {@StringValue}", (object)"Sherlock Holmes");
             logger.Information("Property value is NOT replaced by its type 'DateTime' 1: {@BeforeSpecialDate}",             SpecialDate.AddDays(value: -1));
             logger.Information("Property value is NOT replaced by its type 'DateTime' 2: {@AfterSpecialDate}",              SpecialDate.AddDays(value: +1));
+
+            // Output
+            // [01:57:06 INF] Masked values examples
+            // [01:57:06 INF] Object Destructuring: {"Id": "71570f6a-05f4-4884-970b-34918af003a3", "Name": "*******tson", "Age": 35, "BirthDate": "1875-05-06T00:00:00.0000000", "Car": {"Id": "6
+            // 1b032f1-a71c-452f-b811-c4c9ff98a2c3", "FullName": "***", "Model": "***", "ManufactureDate": "DateTime.Secured", "$type": "Car"}, "Characteristics": {"goodPartner": "***", "brave"
+            // : "***"}, "CarPayment": {"Car { Id: fbd8921c-fd3a-4646-9600-e4b916cbe54f, FullName: \"***\", Model: \"***\", ManufactureDate: 01/01/0001 00:00:00 }": 42000}, "Tags": ["***", "***
+            // ", "***"], "$type": "User"}
+            // [01:57:06 INF] Dictionary Destructuring 1: {"goodPartner": "***", "brave": "***"}
+            // [01:57:06 INF] Dictionary Destructuring 2: {"Car { Id: fbd8921c-fd3a-4646-9600-e4b916cbe54f, FullName: \"***\", Model: \"***\", ManufactureDate: 01/01/0001 00:00:00 }": 42000}
+            // [01:57:06 INF] Property value is replaced by its name 'ReplacedStringValue': +++===+++
+            // [01:57:06 INF] Property value is omitted by its name 'OmittedStringValue' : {@OmittedStringValue}
+            // [01:57:06 INF] ==============================================
+            // [01:57:06 INF] The next values will not be deconstructed or masked, custom deconstruct policy/enrichers can't handle such cases.
+            // [01:57:06 INF] Property value is NOT replaced by its type 'string': Sherlock Holmes
+            // [01:57:06 INF] Property value is NOT replaced by its type 'string' (with cast to object): Sherlock Holmes
+            // [01:57:06 INF] Property value is NOT replaced by its type 'DateTime' 1: 12/31/1904 00:00:00
+            // [01:57:06 INF] Property value is NOT replaced by its type 'DateTime' 2: 01/02/1905 00:00:00
+
         }
 
         private static Logger CreateLogger(IConfiguration configuration)
